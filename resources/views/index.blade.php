821d2195023cd3d313be65bfaefe4032c805fbc2
Santality <saintly231@gmail.com>
2023-11-28 11:34:33 +0500

    index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,14 +10,35 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
+    <script src="/script/flickity.pkgd.min.js"></script>
+    <link rel="stylesheet" href="/style/flickity.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="style/style.css">
 </head>
 
 <body>
-    <x-header></x-header>
-    {{-- <x-footer></x-footer> --}}
+    <div class="bg-index">
+        <x-headerindex></x-headerindex>
+        <div class="container bg-index-block">
+            <h1 class="index-big-title">Музыка - это стенограмма эмоций</h1>
+        </div>
+    </div>
+    <div class="container">
+        <h1 class="new-index-title">НОВЫЕ АЛЬБОМЫ</h1>
+        <div class="gallery js-flickity" data-flickity-options='{ "wrapAround": true }'>
+            <div class="gallery-cell">
+                <a href="">
+                    <img src="/img/habib.png" alt="">
+                    <div class="gallery-cell-text-block">
+                        <p>Егор Летов</p>
+                        <span>Вершки и корешки</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <h1 class="new-index-title">НОВЫЕ РЕЛИЗЫ</h1>
+    </div>
 </body>
 
 </html>
