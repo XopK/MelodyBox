ef6035d47c541617639fba6c31ae39babcf42e4a
Santality <saintly231@gmail.com>
2023-11-28 14:13:58 +0500

    personal area начало

--- resources/views/personal_area.blade.php
+++ resources/views/personal_area.blade.php
@@ -10,7 +10,19 @@
 <body>
     <x-header></x-header>
     <div class="container">
+        <h1 class="personal-area-title">Личный кабинет</h1>
+        <div class="persoanl-area-container">
+            <div class="persoanl-area-container-navigation">
+                <ul class="personal-nav-ul">
+                    <li class="personal-nav p-n-first"><img src="/img/personal.svg" alt="personal.svg"> Персональные данные</li>
+                    <li class="personal-nav"><img src="/img/personal-plus.svg" alt="personal-plus.svg"> Добавить альбом</li>
+                    <li class="personal-nav p-n-last"><img src="/img/personal-exit.svg" alt="personal-exit.svg"> Выход из аккаунта</li>
+                </ul>
+            </div>
+            <div class="personal-profile">
 
+            </div>
+        </div>
     </div>
 </body>
 </html>
