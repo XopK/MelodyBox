89c559718a85702e0fa7508b2392960cc2526870
Santality <saintly231@gmail.com>
2023-11-30 16:29:14 +0500

    profile

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,7 @@
 
 <body>
     <div class="bg-index">
-        <x-header></x-header>
+        <x-headerindex></x-headerindex>
         <div class="container bg-index-block">
             <h1 class="index-big-title" >Музыка - это стенограмма эмоций</h1>
         </div>
