7a6d95476565e95a774ea07aacaf4dd7e5649a45
QuardSA <smirnov_aleksey04@inbox.ru>
2023-12-15 13:21:33 +0500

    Test case complite

--- resources/views/components/registration.blade.php
+++ resources/views/components/registration.blade.php
@@ -10,27 +10,81 @@
                         @csrf
                         <label for="name-form" class="form-label">Имя</label>
                         <input type="text" name="reg_name" class="form-text mt-1" id="name-form">
+                        @error('reg_name')
+                            <div class="alert alert-danger alert-dismissible">
+                                <div class="alert-text">
+                                    {{ $message }}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                                </div>
+                            </div>
+                        @enderror
 
                         <label for="surname-form" class="form-label">Фамилия</label>
                         <input type="text" name="reg_surname" class="form-text mt-1" id="surname-form">
+                        @error('reg_surname')
+                            <div class="alert alert-danger alert-dismissible">
+                                <div class="alert-text">
+                                    {{ $message }}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                                </div>
+                            </div>
+                        @enderror
 
                         <label for="number-form" class="form-label">Номер телефона</label>
                         <input type="text" name="phone_number" class="form-text mt-1" id="number-form">
+                        @error('phone_number')
+                            <div class="alert alert-danger alert-dismissible">
+                                <div class="alert-text">
+                                    {{ $message }}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                                </div>
+                            </div>
+                        @enderror
 
                         <label for="email-form" class="form-label">Электронная почта</label>
                         <input type="email" name="email" class="form-text mt-1" id="email-form">
+                        @error('email')
+                            <div class="alert alert-danger alert-dismissible">
+                                <div class="alert-text">
+                                    {{ $message }}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                                </div>
+                            </div>
+                        @enderror
 
                         <label for="password-form" class="form-label">Пароль</label>
                         <input type="password" name="reg_password" class="form-text mt-1" id="password-form">
+                        @error('reg_password')
+                            <div class="alert alert-danger alert-dismissible">
+                                <div class="alert-text">
+                                    {{ $message }}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                                </div>
+                            </div>
+                        @enderror
 
                         <label for="password-form" class="form-label mt-2">Подтверждение пароля</label>
                         <input type="password" name="reg_confirm_password" class="form-text" id="password-form">
+                        @error('reg_confirm_password')
+                            <div class="alert alert-danger alert-dismissible">
+                                <div class="alert-text">
+                                    {{ $message }}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                                </div>
+                            </div>
+                        @enderror
 
                         <button type="submit" class="button-form mt-3">Создать аккаунт</button>
                     </form>
 
                     <div class="d-flex justify-content-center fs-4 mt-1">Есть аккаунт? <p><a data-bs-toggle="modal" data-bs-target="#Authorization">Войдите!</a>
+                                                @if(session('succes'))
+                        <div class="alert alert-success" role="alert">
+                            {{session('succes')}}
+                        </div>
+                        @endif
                         </p>
+
                     </div>
 
                 </div>
